761a3871732abf71d3d5bd582984974bf330e243
Arthur Thieffry <arthieffry@gmail.com>
2017-11-28 15:45:07 +0100

    Création des controllers et routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :profiles, only: [:new, :create, :edit, :update, :delete]
+
+  resources :chevals do
+    resources :reviews, only: [:new, :create, :edit, :update, :delete]
+    resources :bookings, only: [:show, :new, :create, :edit, :update, :delete]
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
