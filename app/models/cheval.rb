45d941b8f6302e7fb59b193ec568bd703f2592ff
Romain Capelle <romain.capelle@gmail.com>
2017-11-28 15:04:54 +0100

    creation of Dependances and Seeds

--- app/models/cheval.rb
+++ app/models/cheval.rb
@@ -2,4 +2,13 @@ class Cheval < ApplicationRecord
   belongs_to :profile
   belongs_to :robe
   belongs_to :category
+
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :description, presence: true, length: { maximum:2500 }
+  validates :address, presence: true
+  validates :taille, presence: true
+
+  has_many :profiles, through: :bookings
+  has_many :reviews
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :profile
   belongs_to :cheval
+  
+  validates_numericality_of :rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, default: 0, presence: true
+  validates :content, length: { maximum: 150 }
 end
+
+
