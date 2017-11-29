5f7d295ca6f9953ff862f29bca031503d9b04f57
Claire Soullie <cmp.soullie@gmail.com>
2017-11-29 18:02:42 +0100

    correction creation profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :profile_present?
+
+ private
+
+   def profile_present?
+     redirect_to new_profile_path if current_user && current_user.profile.nil?
+     # unless user_signed_in? && current_user.profile.present?
+   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :profile_present?
   def home
   end
 end
